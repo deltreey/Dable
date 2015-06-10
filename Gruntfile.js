a213d403b437325ea923197f210877bf19ac96f9
deltreey <suicidolt@gmail.com>
2015-06-10 02:14:54 -0400

    making travis tests run

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        
         jshint: {
             all: {
                 src: ['Dable.js'],
@@ -16,13 +16,16 @@ module.exports = function(grunt) {
                     }
                 }
             }
+        },
+        qunit: {
+            all: ['test/test.html']
         }
     });
 
     if (process.env.NODE_ENV === 'development') {
         grunt.loadNpmTasks('grunt-contrib-jshint');
 
-        grunt.registerTask('test', ['jshint']);
+        grunt.registerTask('test', ['jshint', 'qunit']);
         grunt.registerTask('default', 'test');
     }
 };
